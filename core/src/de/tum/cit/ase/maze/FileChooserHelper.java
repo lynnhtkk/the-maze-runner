13861d6cd5ed4c0383ce8349e8273cc7d5ab3e94
Nyi Nyi Linn Htet <nyinyi.linn-htet@tum.de>
2024-01-29 02:38:18 +0100

    Add documentations (JavaDoc comments) to necessary methods in FileChooserHelper class and GameState enumeration

--- core/src/de/tum/cit/ase/maze/FileChooserHelper.java
+++ core/src/de/tum/cit/ase/maze/FileChooserHelper.java
@@ -1,17 +1,35 @@
 package de.tum.cit.ase.maze;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.files.FileHandle;
 import games.spooky.gdx.nativefilechooser.NativeFileChooserConfiguration;
 
+/**
+ * Utility class to assist with file choosing operations, particularly for selecting map files.
+ * <p>
+ * This class provides static methods to configure file chooser dialogs used in the game,
+ * ensuring that players can select map files efficiently and correctly. The configurations
+ * are tailored to the specific needs of the game, such as filtering for specific file types
+ * and setting appropriate default directories.
+ * </p>
+ */
 public class FileChooserHelper {
+
+    /**
+     * Creates and returns a configuration for a native file chooser specific to selecting map files.
+     * <p>
+     * This method sets up a {@link NativeFileChooserConfiguration} object for choosing map files.
+     * It specifies the directory to start in, sets a name filter to only display files with a
+     * '.properties' extension, and sets the title of the file chooser dialog. This configuration
+     * is intended to facilitate the user in selecting the correct map file for the game.
+     * </p>
+     *
+     * @return A pre-configured {@link NativeFileChooserConfiguration} object for selecting map files.
+     */
     public static NativeFileChooserConfiguration mapChooserConfiguration() {
         NativeFileChooserConfiguration conf = new NativeFileChooserConfiguration();
-        conf.directory = Gdx.files.absolute(System.getProperty("user.home"));
         conf.directory = Gdx.files.internal("maps");
         conf.nameFilter = (dir, name) -> name.endsWith(".properties");
         conf.title = "Choose Map";
         return conf;
     }
-
 }
--- core/src/de/tum/cit/ase/maze/GameState.java
+++ core/src/de/tum/cit/ase/maze/GameState.java
@@ -1,5 +1,19 @@
 package de.tum.cit.ase.maze;
 
+/**
+ * Enumeration representing the various states of the game.
+ * <p>
+ * This enumeration is used to define the different stages that the game can be in
+ * at any given time. It is essential for controlling game flow, determining when to load new maps,
+ * and managing transitions between different states of gameplay. The possible states are:
+ * <ul>
+ *     <li>{@code RUNNING}: The game is in progress and ongoing.</li>
+ *     <li>{@code NEW_GAME}: A new game is to be started, typically requiring initialization a new {@link GameScreen}.</li>
+ *     <li>{@code GAME_OVER}: The game has ended due to the player losing or other conditions.</li>
+ *     <li>{@code VICTORY}: The game has ended with the player achieving victory.</li>
+ * </ul>
+ * </p>
+ */
 public enum GameState {
     RUNNING,
     NEW_GAME,
